915773c8ab6e8ea6132fb463b61ac54094f5bdab
Paweł Musioł <p.musiol1999@gmail.com>
2021-08-05 21:40:33 +0200

    Add Illustrations

--- components/molecules/gallery-category.js
+++ components/molecules/gallery-category.js
@@ -3,10 +3,10 @@ const GalleryCategory = ({setCategory}) => {
 	return(
 		<div className="gallery-category">
 			<button onClick={() => setCategory(0)} >Wszystko</button>
-			<button onClick={() => setCategory(1)} >kategoria 1</button>
-			<button onClick={() => setCategory(2)} >kategoria 2</button>
-			<button onClick={() => setCategory(3)} >kategoria 3</button>
-			<button onClick={() => setCategory(4)} >kategoria 4</button>
+			<button onClick={() => setCategory(1)} >Portraits</button>
+			<button onClick={() => setCategory(2)} >Characters Design</button>
+			<button onClick={() => setCategory(3)} >Illustrations</button>
+			<button onClick={() => setCategory(4)} >Concept Arts</button>
 		</div>
 	)
 }
